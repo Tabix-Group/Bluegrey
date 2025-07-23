c61836509b6f4e4dd9c40f30b35a56c58b744e55
hernanofx <hernandorra@gmail.com>
2025-07-23 15:50:11 -0300

    redeploy 3

--- backend/index.js
+++ backend/index.js
@@ -12,10 +12,7 @@ import productosRouter from './routes/productos.js';
 import cronogramasRouter from './routes/cronogramas.js';
 import entregasRouter from './routes/entregas.js';
 import modelosMensajeRouter from './routes/modelos_mensaje.js';
-// Fix for CommonJS export in usuariosRouter
-import { createRequire } from 'module';
-const require = createRequire(import.meta.url);
-const usuariosRouter = require('./routes/usuarios.js');
+import usuariosRouter from './routes/usuarios.js';
 
 import cronogramasExtraRouter from './routes/cronogramas_extra.js';
 
--- backend/routes/usuarios.js
+++ backend/routes/usuarios.js
@@ -1,10 +1,10 @@
-const express = require('express');
-const router = express.Router();
-const usuariosCtrl = require('../controllers/usuarios');
+import express from 'express';
+import * as usuariosCtrl from '../controllers/usuarios.js';
 
+const router = express.Router();
 router.get('/', usuariosCtrl.getUsuarios);
 router.post('/', usuariosCtrl.createUsuario);
 router.put('/:id', usuariosCtrl.updateUsuario);
 router.delete('/:id', usuariosCtrl.deleteUsuario);
 
-module.exports = router;
+export default router;
